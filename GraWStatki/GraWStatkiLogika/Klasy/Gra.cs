c881f2e6f79a13828fc580ea4612646449b05dd0
FilipKotelon <off.f.kotelon@gmail.com>
2019-12-14 15:47:08 +0100

    Rozpoczęcie tworzenia logiki gry i próbne dwa gridy na pola bitwy

---
+++ GraWStatki/GraWStatkiLogika/Klasy/Gra.cs
@@ -0,0 +1,24 @@
+﻿using GraWStatkiFront;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraWStatkiLogika
+{
+    class Gra
+    {
+        public string zwyciezca;
+        private string _gracz;
+        private string _komputer = "Komputer";
+
+        private PlanszaBitwy _planszaGracza;
+        private PlanszaBitwy _planszaKomputera;
+
+        public Gra()
+        {
+            _gracz = "Gracz";
+        }
+    }
+}
