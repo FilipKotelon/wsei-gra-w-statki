a7c0c4c769b5e22dbb447674a3845d8b0b268d23
Filip <off.f.kotelon@gmail.com>
2020-01-19 22:24:20 +0100

    Pierwsze testy

---
+++ GraWStatki/GraWStatkiTesty/KontrolaGry/L_KontrolerGryTesty.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraWStatkiLogika.KontrolaGry;
+
+namespace GraWStatkiTesty.KontrolaGry
+{
+    [TestClass]
+    public class L_KontrolerGryTesty
+    {
+        [TestMethod]
+        public void ZmienTure_TuraZostalaZmienionaILicznikTurZostalPowiekszony()
+        {
+            //Przygotowanie
+            L_KontrolerGry kontroler = new L_KontrolerGry();
+            bool czyTuraGracza = kontroler.CzyTuraGracza;
+            int poprzedniaTura = kontroler.LicznikTur;
+
+            //Działanie
+            kontroler.ZmienTure();
+
+            //Sprawdzenie
+            Assert.IsTrue(czyTuraGracza != kontroler.CzyTuraGracza && poprzedniaTura < kontroler.LicznikTur);
+        }
+
+
+        //Todo
+        [TestMethod]
+        public void SprawdzRuch_NieTrafionoStatku_TuraZostalaZmienionaILicznikTurZostalPowiekszony()
+        {
+            //Przygotowanie
+            L_KontrolerGry kontroler = new L_KontrolerGry();
+            bool czyTuraGracza = kontroler.CzyTuraGracza;
+            int poprzedniaTura = kontroler.LicznikTur;
+
+            //Działanie
+            //SprawdzRuch
+
+            //Sprawdzenie
+            Assert.IsTrue(czyTuraGracza != kontroler.CzyTuraGracza && poprzedniaTura < kontroler.LicznikTur);
+        }
+
+        //Todo
+        [TestMethod]
+        public void SprawdzRuch_TrafionoStatek_TuraNieZostalaZmienionaILicznikTurNieZostalPowiekszony()
+        {
+            //Przygotowanie
+            L_KontrolerGry kontroler = new L_KontrolerGry();
+            bool czyTuraGracza = kontroler.CzyTuraGracza;
+            int poprzedniaTura = kontroler.LicznikTur;
+
+            //Działanie
+            //SprawdzRuch
+
+            //Sprawdzenie
+            //Sprawdź czy są równe temu co było
+            //Assert.IsTrue();
+        }
+
+        //Todo
+        [TestMethod]
+        public void ZakonczGre_TuraGracza_ZwyciezcaToGracz()
+        {
+            //Przygotowanie
+            L_KontrolerGry kontroler = new L_KontrolerGry();
+            //Tworzysz nową grę kontroler.NowaGra()
+            //Na początku jest tura gracza więc tu jest ok
+
+            //Działanie
+            //ZakonczGre
+
+            //Sprawdzenie
+            //Assert czy zwyciezca == "Gracz"
+        }
+
+        //Todo
+        [TestMethod]
+        public void ZakonczGre_TuraKomputera_ZwyciezcaToKomputer()
+        {
+            //Przygotowanie
+            //To samo co wyżej, tylko w działaniu najpierw zmieniasz turę, żeby była komputera
+
+            //Działanie
+            //A potem kończysz
+
+            //Sprawdzenie
+            //Assert czy zwyciezca == "Komputer"
+        }
+    }
+}
---
+++ GraWStatki/GraWStatkiTesty/PlanszaBitwy/L_PlanszaBitwyTesty.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraWStatkiTesty.PlanszaBitwy
+{
+    /// <summary>
+    /// Summary description for L_PlanszaBitwyTesty
+    /// </summary>
+    [TestClass]
+    public class L_PlanszaBitwyTesty
+    {
+        //Todo
+        [TestMethod]
+        public void WypelnijPustePola_NieMaPustychPol()
+        {
+            //Przygotowanie
+            //Zrób sobie nową L_PlanszaBitwy i pobierz jej pola, np pola = plansza.Pola
+            //Potem w działaniu sprawdź czy wszystkie pola są puste
+
+            //Działanie
+            //Daj flagę przed, np. bool polaSaPuste = true;
+            //Przy kazdym przejsciu petli, jesli plansza.Pola[i, j] != null to polaSaPuste = false
+
+            //Sprawdzenie
+            //Assert czy polaSaPuste to true
+        }
+
+        //Todo
+        [TestMethod]
+        public void DodajStatek_DodanoJedenStatek()
+        {
+            //Przygotowanie
+            //Zrób sobie nową L_PlanszaBitwy i pobierz jej statki
+            //Pobierz długość listy ze statkami (obecnaDlugosc = lista.Count)
+            //Stwórz nowy statek (np. w pliku L_BudowniczyStatków, linia 399)
+
+            //Działanie
+            //Dodaj statek funkcją
+
+            //Sprawdzenie
+            //Assert czy długość listy statków się powiększyła
+        }
+    }
+}
